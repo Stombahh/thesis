6fb579815fecdef52733512c44b871a18a2c01ec
stombahh <tommi.ilvonen@gmail.com>
2018-03-23 21:49:36 +0200

    Took off unnecessary vue stuff, changed bg and navbar color.

--- kallengalleria/src/App.vue
+++ kallengalleria/src/App.vue
@@ -1,72 +1,61 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+<v-app>
+  <!-- <v-navigation-drawer persistent :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher fixed app>
+    <v-list>
+      <v-list-tile value="true" v-for="(item, i) in items" :key="i">
+        <v-list-tile-action>
+          <v-icon v-html="item.icon"></v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.title"></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer> -->
+
+  <!-- start: navbar -->
+  <v-toolbar app :clipped-left="clipped" :dark="true">
+    <v-toolbar-title v-text="title"></v-toolbar-title>
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+    </v-btn>
+    <v-btn icon @click.stop="clipped = !clipped">
+      <v-icon>web</v-icon>
+    </v-btn>
+    <v-btn icon @click.stop="fixed = !fixed">
+      <v-icon>remove</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+  <!-- end: navbar -->
+
+  <!-- start: content -->
+  <v-content>
+    <router-view/>
+  </v-content>
+  <!-- end: content -->
+
+  <!-- start: right side navbar -->
+  <!-- <v-navigation-drawer temporary :right="right" v-model="rightDrawer" fixed app>
+    <v-list>
+      <v-list-tile @click="right = !right">
+        <v-list-tile-action>
+          <v-icon>compare_arrows</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer> -->
+  <!-- end: right side navbar -->
+
+  <!-- start: footer -->
+  <v-footer :fixed="fixed" :dark="true" app>
+    <span>&copy; 2018</span>
+  </v-footer>
+  <!-- end: footer -->
+
+</v-app>
 </template>
 
 <script>
@@ -83,7 +72,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'Kalle Pitkänen',
     };
   },
   name: 'App',
